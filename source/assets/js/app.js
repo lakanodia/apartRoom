75eea887951ba5316775f279d116834e77f2f2ef
Demur <demur.nodia@gmail.com>
2024-02-24 21:35:48 +0400

    Include JS

--- source/assets/js/app.js
+++ source/assets/js/app.js
@@ -2,7 +2,6 @@
 // import functions
 import classlist_polyfill from "./modules/classlist_polyfill";
 import for_each_polyfill from "./modules/foreach_polyfill";
-import focus_accessibility from "./modules/focus_accessibility";
 import type_ratio_interpolation from "./modules/type_ratio_interpolation";
 /*
     polyfill to enable use of forEach on node lists in IE11
@@ -18,7 +17,3 @@ classlist_polyfill();
     and store the result in a CSS custom property
 */
 type_ratio_interpolation();
-/*
-    allow enchanced focus detection (depends on a11y.js)
-*/
-focus_accessibility();
